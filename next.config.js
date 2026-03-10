c203eb81da85d33c1cf430992705c6904ce91968
Rohit Singh <100846587+rohitsdev4@users.noreply.github.com>
2026-03-10 18:56:49 +0530

    fix: remove COEP/COOP headers blocking API calls

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    // Required headers for WebGPU & SharedArrayBuffer
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Cross-Origin-Embedder-Policy",
-                        value: "require-corp",
-                    },
-                    {
-                        key: "Cross-Origin-Opener-Policy",
-                        value: "same-origin",
-                    },
-                ],
-            },
-        ];
-    },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
